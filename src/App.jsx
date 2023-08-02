2b6af2be92151e737f2183704ee1ab469fc165af
Francisco Berti da Cruz <fbc991@live.com>
2023-08-02 16:15:25 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  HomeLayout,
+  About,
+  Landing,
+  Error,
+  Newsletter,
+  Cocktail,
+} from './pages';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />
+  },
+  {
+    path: '/about',
+    element: <About />
+  }
+])
+
+const App = () => {
+  return <RouterProvider router={router} />
+};
+
+export default App;
